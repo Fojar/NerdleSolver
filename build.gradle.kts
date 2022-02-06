1f352eef142c8a8cdff283e788b4f54639fcae10
Aravin <aravin@mentis.ca>
2022-02-06 17:47:24 -0500

    Add basic Gradle application

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use Kotlin Test test framework
+            useKotlinTest()
+        }
+    }
+}
+
+application {
+    mainClass.set("nerdlesolver.NerdleSolverKt")
+}
---
+++ src/main/kotlin/nerdlesolver/NerdleSolver.kt
@@ -0,0 +1,29 @@
+package nerdlesolver
+
+
+fun main() {
+	println("Nerdle Solver")
+}
+
+
+fun isValidNerdle(s: String): Boolean {
+
+	if (s.length != 8) return false
+
+	val indexOfEquals = s.indexOfEquals() ?: return false
+
+	val left = s.substring(0 until indexOfEquals)
+	val right = s.substring(indexOfEquals + 1)
+
+
+	return true
+}
+
+
+// There must be an equals sign in the string. The farthest to the right it can be is index 6,
+// because there must be at least 1 digit after it. The farthest to the left it can be is index 4,
+// because there's no way to get a 4-digit number from an expression with only 3 characters given
+// the operators available. This returns the index of the first equals sign if it is in that range,
+// otherwise null. It does not care if there are multiple equals signs.
+private fun String.indexOfEquals() = indexOf('=').let { if (it in 4..6) it else null }
+
