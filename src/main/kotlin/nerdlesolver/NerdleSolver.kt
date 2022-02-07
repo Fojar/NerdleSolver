91024b3dbd6e614a142cc32b0eacd9a087d5c190
Aravin <aravin@mentis.ca>
2022-02-06 21:35:36 -0500

    Valid nerdles have a number to the right of equals sign

--- src/main/kotlin/nerdlesolver/NerdleSolver.kt
+++ src/main/kotlin/nerdlesolver/NerdleSolver.kt
@@ -15,15 +15,25 @@ fun isValidNerdle(s: String): Boolean {
 	val left = s.substring(0 until indexOfEquals)
 	val right = s.substring(indexOfEquals + 1)
 
+	if (!right.isNumber()) return false
+
 
 	return true
 }
 
 
-// There must be an equals sign in the string. The farthest to the right it can be is index 6,
-// because there must be at least 1 digit after it. The farthest to the left it can be is index 4,
-// because there's no way to get a 4-digit number from an expression with only 3 characters given
-// the operators available. This returns the index of the first equals sign if it is in that range,
-// otherwise null. It does not care if there are multiple equals signs.
+/*
+Standard Java string parsing allows leading zeroes, but for this domain we disallow it.
+ */
+private fun String.isNumber() = this[0] != '0' && toIntOrNull() != null
+
+
+/*
+There must be an equals sign in the string. The farthest to the right it can be is index 6,
+because there must be at least 1 digit after it. The farthest to the left it can be is index 4,
+because there's no way to get a 4-digit number from an expression with only 3 characters given
+the operators available. This returns the index of the first equals sign if it is in that range,
+otherwise null. It does not care if there are multiple equals signs.
+*/
 private fun String.indexOfEquals() = indexOf('=').let { if (it in 4..6) it else null }
 
